bcb697d330d3a737da6e9eb382b7d6d662132c95
RopeAnime123 <103525011+RopeAnime123@users.noreply.github.com>
2024-11-30 01:09:27 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -54,8 +54,18 @@ app.all('/player/growid/login/validate', (req, res) => {
     );
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+//app.get('/', function (req, res) {
+   // res.send('helo world');
+//});
+app.all('/', function (req, res) {
+    const tData = {};
+    try {
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+    } catch (why) { console.log(`Warning: ${why}`); }
+
+    res.render(__dirname + '/public/html/dashboard.html', { data: tData });
 });
 
 app.listen(5000, function () {
